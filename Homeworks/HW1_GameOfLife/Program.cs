fdf31bf4114fa084809390fe7b8724e58d86692b
Erika Mesh <1838833+esmesh@users.noreply.github.com>
2025-01-22 10:44:07 -0500

    Initial commit

---
+++ Homeworks/HW1_GameOfLife/Program.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * YOU_NAME_HERE
+ * 
+ * HW 1 - Game of Life: https://docs.google.com/document/d/13zQjG7a25GUA1Oh6pwQXUtF6lJTJscWuuL-wwnMBSIo/edit?usp=sharing
+ * 
+ * Release Notes:
+ *  - 
+ * 
+ */
+namespace HW1_GameOfLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

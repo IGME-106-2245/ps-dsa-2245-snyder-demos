ece5ab7efc2610cfebd8b302944d9b6889289bef
Benjamin Snyder <bwsigm@rit.edu>
2025-02-05 10:57:14 -0500

    In Class Demos

---
+++ Demos/InterfaceDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace InterfaceDemo
+{
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeekDays today = WeekDays.Monday;
+
+
+            if (today == WeekDays.Monday)
+            {
+                Console.WriteLine("I tolerate Mondays!");
+            }
+        }
+    }
+}
